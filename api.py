d5314089f999919a57c6bfe7e3b86360d440158f
atrox3d <atrox3d@gmail.com>
2022-09-16 10:38:34 +0200

    198. Returning a JSON Response

--- api.py
+++ api.py
@@ -4,22 +4,41 @@ import json
 
 
 class Api:
-    """Handles requests at /api?w=word
+    """
+    Handles requests at /api?w=word
     """
     @classmethod
     def serve(cls, req):
-        wp = jp.WebPage()
-        word = req.query_params.get('w')
-        print(word)
-        jp.Div(a=wp, text=word.title())
-        # defined = definition.Definition(word).get()
-        #
-        # response = {
-        #     "word": word,
-        #     "definition": defined
-        # }
-        #
-        # wp.html = json.dumps(response)
+        wp = jp.WebPage()                                       # create justpy webpage
+        word = req.query_params.get('w')                        # get query-string ?w={word}
+
+        """
+        this way of returning word has the downside of returning
+        all of html and javascript along with {word}
+        """
+        # print(word)
+        # jp.Div(a=wp, text=word.title())                       # print word into div
+
+        """
+        this other way assigns only the {word} string to wp.html
+        """
+        # print(f'wp.html: {wp.html}')
+        # print(f'?w: {word}')
+        # wp.html = word
+        # print(f'wp.html: {wp.html}')
+
+        defined = definition.Definition(word).get()
+        print(f'?w         | {word}')
+        print(f'definition | {defined}')
+
+        response = {                                            # create dict for response
+            "word": word,
+            "definition": defined
+        }
+
+        wp.html = json.dumps(response)                          # converto to string and assing
+        print(f'wp.html    | {wp.html}')
+
         return wp
 
 
